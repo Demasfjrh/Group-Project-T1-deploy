a3b0b7ecdb542bdc746b5561e1a2fbaa830ba6b5
Demasfj <168269824+Demasfjrh@users.noreply.github.com>
2025-03-06 17:14:16 +0700

    Update socket.js

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = 'http://localhost:3000'; // Sesuaikan dengan URL server Anda
+const SOCKET_URL = '18.140.70.235'; // Sesuaikan dengan URL server Anda
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false,
